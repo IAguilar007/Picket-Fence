9c73fc4f5b5ae4d74f8c6654d1dea298fe8b6132
Erik von Reis <evonreis@caltech.edu>
2026-02-11 14:13:39 -0800

    LHO: subprocess.Popen wasn't working anymore, so LHO-Server.py now calls os.execv() to start the process

--- LHO-Server.py
+++ LHO-Server.py
@@ -2,15 +2,20 @@
 
 import subprocess
 import sys
+import os
+
 
 def main():
     """
-    THIS PROGRAM JUST CALLS THE EPICS SERVER V2 AND IT IS STRUCTURED THIS WAY FOR BACKWARDS COMPATIBILITY
+    THIS PROGRAM JUST CALLS THE EPICS SERVER V2 AND IT IS
+    STRUCTURED THIS WAY FOR BACKWARDS COMPATIBILITY
     """
-    
+
+    print("starting lho picket fence server")
     prefix = 'H1:SEI-USGS_'
-    path= "Picket_fence_EPICS_server_v2.py"
-    subprocess.Popen(['python3', path, prefix])
+    path = "Picket_fence_EPICS_server_v2.py"
+    os.execv('/usr/bin/python3', ['/usr/bin/python3', path, prefix])
+    print(f"picket fence closed with return code {result.return_code}")
     sys.exit(0)
 
 
